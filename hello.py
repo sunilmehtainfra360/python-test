5b0602bca61fbaa3d7c8630b6f88721306420117
sunilmehtainfra360 <114050271+sunilmehtainfra360@users.noreply.github.com>
2022-10-02 14:03:50 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,1 +1,11 @@
-print('Hello World')
+import os # For environmental variables when running in CodeBuild, Fargate, Lambda, etc.
+import boto3 # Because you need it lol
+import botocore # For Error Handling
+import json # To parse "stringified" JSON Policy documents
+import time # to create Unix timestamps for DynamoDB TTL
+import multiprocessing
+import hashlib # To create unique IDs for places where AWS doesn't have them
+from botocore.config import Config
+
+for bucket in s3_resource.buckets.all():
+        print(bucket.name)
